653c8bb93b9400c01262bf05939f809a4a818a87
EDV-Beratung <kontakt@edv-beratung.familiezimmermann.de>
2019-01-26 12:56:25 +0100

    Rechnungstyp Remittende ergänzt

--- Modulhelferlein.java
+++ Modulhelferlein.java
@@ -149,8 +149,9 @@ public class Modulhelferlein {
     public static String[] SpracheRechnung = {"Rechnung", "Invoice"};
     public static String[] SpracheMahnung = {"Mahnung", "Reminder"};
     public static String[] SpracheGeschenk = {"Geschenk", "Gift"};
-    public static String[] SpracheRezensionsexemplar = {"Rezensionsexemplar", ""};
-    public static String[] SpracheBelegexemplar = {"Belegexemplar", "author's copy"};
+    public static String[] SpracheRezensionsexemplar = {"Rezensionsexemplar", "For Review"};
+    public static String[] SpracheRemittende = {"Remittende", "Replacement"};
+    public static String[] SpracheBelegexemplar = {"Belegexemplar", "Author's copy"};
     public static String[] SprachePflichtexempar = {"Pflichtexemplar", ""};
     public static String[] SpracheRechnungsnummer = {"Rechnungsnummer", "Invoice No."};
     public static String[] SpracheRechnungsdatum = {"Rechnungsdatum", "Date of Invoice"};
