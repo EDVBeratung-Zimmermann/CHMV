c2b1b1dfe7880da751cd1c011a211e9668dc564c
tzimmermann <thomas@familiezimmermann.de>
2020-04-15 19:59:13 +0200

    License header aktualisiert überflüssigen Code entfernt

--- CustomOutputStream.java
+++ CustomOutputStream.java
@@ -1,14 +1,30 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
+ * Das JAVA-Programm Miles-Verlag Verlagsverwaltung stellt alle notwendigen
+ * Funktionen für die Verwaltung des Carola Hartman Miles-Verlags bereit.
+ *
+ * Copyright (C) 2017 EDV-Beratung und Betrieb, Entwicklung von Software
+ *                    Dipl.Inform Thomas Zimmermann
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 package milesVerlagMain;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
 
 /**
  * This class extends from OutputStream to redirect output to a JTextArrea
@@ -18,7 +34,7 @@ import javax.swing.SwingUtilities;
  */
 public class CustomOutputStream extends OutputStream {
 
-    private JTextArea textArea;
+    private final JTextArea textArea;
 
     /**
      *
@@ -29,20 +45,12 @@ public class CustomOutputStream extends OutputStream {
     }
 
     @Override
-    //
     public void write(int b) throws IOException {
         // redirects data to the text area
         textArea.append(String.valueOf((char) b));
+        
         // scrolls the text area to the end of data
         textArea.setCaretPosition(textArea.getDocument().getLength());
     }
     
-    //public void write(int b) throws IOException {
-    //    SwingUtilities.invokeLater(new Runnable(){ 
-    //        public void run(){
-    //            textArea.append(String.valueOf((char) b));
-    //            textArea.setCaretPosition(textArea.getDocument().getLength()); } }
-    //    );
-    //}
-
 }
