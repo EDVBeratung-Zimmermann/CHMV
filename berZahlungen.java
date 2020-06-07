5d24229f154240b50a913df8812b1dc32a7640ce
tzimmermann <thomas@familiezimmermann.de>
2020-06-07 14:10:48 +0200

    unused imports entfernt, prüfen auf Corona-Ermäßigung

--- berZahlungen.java
+++ berZahlungen.java
@@ -30,9 +30,8 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Calendar;
-import static milesVerlagMain.Modulhelferlein.Ausgabe;
-import static milesVerlagMain.Modulhelferlein.AusgabeDB;
-import static milesVerlagMain.Modulhelferlein.Linie;
+import static milesVerlagMain.ModulHelferlein.AusgabeDB;
+import static milesVerlagMain.ModulHelferlein.Linie;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDDocumentInformation;
@@ -41,6 +40,7 @@ import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import static org.apache.pdfbox.pdmodel.common.PDRectangle.A4;
 import org.apache.pdfbox.pdmodel.font.PDFont;
 import org.apache.pdfbox.pdmodel.font.PDType1Font;
+import static milesVerlagMain.ModulHelferlein.AusgabeLB;
 
 /**
  * Klasse zur Erzeugung einer Liste von offenen Ausgaben
@@ -58,7 +58,7 @@ public class berZahlungen {
      * @param strBis Ende des Zeitraums
      */
     public static void berichtXLS(String strVon, String strBis) {
-        Modulhelferlein.Infomeldung("Diese Funktion ist noch nicht implementiert!");
+        ModulHelferlein.Infomeldung("Diese Funktion ist noch nicht implementiert!");
     }
 
     /**
@@ -69,7 +69,7 @@ public class berZahlungen {
      * @param strBis Ende des Zeitraums
      */
     public static void berichtDOC(String strVon, String strBis) {
-        Modulhelferlein.Infomeldung("Diese Funktion ist noch nicht implementiert!");
+        ModulHelferlein.Infomeldung("Diese Funktion ist noch nicht implementiert!");
     }
 
     /**
@@ -92,9 +92,9 @@ public class berZahlungen {
         PDPageContentStream cos;
 
         String outputFileName;
-        outputFileName = Modulhelferlein.pathBerichte + "\\Ausgaben\\"
+        outputFileName = ModulHelferlein.pathBerichte + "\\Ausgaben\\"
                 + "Liste-Zahlungen-"
-                + Modulhelferlein.printSimpleDateFormat("yyyyMMdd") 
+                + ModulHelferlein.printSimpleDateFormat("yyyyMMdd") 
                 + ".pdf";
 
         PDDocumentInformation docInfo = document.getDocumentInformation();
@@ -115,27 +115,27 @@ public class berZahlungen {
             Integer zeile = 1;
             Integer seite = 1;
 
-            Ausgabe(cos, fontBold, 12, Color.BLACK, 56, 770, "Übersicht der offenen Rechnungen/Ausgaben im Zeitraum " + strVon + " - " + strBis);
-            Ausgabe(cos, fontBold, 12, Color.BLACK, 56, 755, "Stand " + Modulhelferlein.printSimpleDateFormat("dd.MM.yyyy"));
-            Ausgabe(cos, fontBold, 12, Color.BLACK, 500, 755, "Seite " + Integer.toString(seite));
-            Ausgabe(cos, fontBold, 12, Color.BLACK, 56, 735, "RechNr");
-            Ausgabe(cos, fontBold, 12, Color.BLACK, 200, 735, "Lieferant");
-            Ausgabe(cos, fontBold, 12, Color.BLACK, 500, 735, "Betrag");
+            AusgabeLB(cos, fontBold, 12, Color.BLACK, 56, 770, "Übersicht der offenen Rechnungen/Ausgaben im Zeitraum " + strVon + " - " + strBis);
+            AusgabeLB(cos, fontBold, 12, Color.BLACK, 56, 755, "Stand " + ModulHelferlein.printSimpleDateFormat("dd.MM.yyyy"));
+            AusgabeLB(cos, fontBold, 12, Color.BLACK, 500, 755, "Seite " + Integer.toString(seite));
+            AusgabeLB(cos, fontBold, 12, Color.BLACK, 56, 735, "RechNr");
+            AusgabeLB(cos, fontBold, 12, Color.BLACK, 200, 735, "Lieferant");
+            AusgabeLB(cos, fontBold, 12, Color.BLACK, 500, 735, "Betrag");
 
             Linie(cos,2,56, 730, 539, 730);
             
             Connection conn = null;
 
             try { // Datenbank-Treiber laden
-                Class.forName(Modulhelferlein.dbDriver);
+                Class.forName(ModulHelferlein.dbDriver);
             } catch (ClassNotFoundException exept) {
-                Modulhelferlein.Fehlermeldung("ClassNotFoundException: Treiber nicht gefunden: " + exept.getMessage());
+                ModulHelferlein.Fehlermeldung("ClassNotFoundException: Treiber nicht gefunden: " + exept.getMessage());
             } // try Datenbank-Treiber laden
 
             try { // Verbindung zur Datenbank über die JDBC-Brücke
-                conn = DriverManager.getConnection(Modulhelferlein.dbUrl, Modulhelferlein.dbUser, Modulhelferlein.dbPassword);
+                conn = DriverManager.getConnection(ModulHelferlein.dbUrl, ModulHelferlein.dbUser, ModulHelferlein.dbPassword);
             } catch (SQLException exept) {
-                Modulhelferlein.Fehlermeldung("SQL-Exception: Verbindung nicht moeglich: " + exept.getMessage());
+                ModulHelferlein.Fehlermeldung("SQL-Exception: Verbindung nicht moeglich: " + exept.getMessage());
             } // try Verbindung zur Datenbank über die JDBC-Brücke
 
             if (conn != null) {
@@ -167,12 +167,12 @@ public class berZahlungen {
                             zeile = 1;
                             seite = seite + 1;
 
-                            Ausgabe(cos, fontBold, 12, Color.BLACK, 56, 770, "Übersicht der offenen Rechnungen/Ausgaben im Zeitraum " + strVon + " - " + strBis);
-                            Ausgabe(cos, fontBold, 12, Color.BLACK, 56, 755, "Stand " + Modulhelferlein.printSimpleDateFormat("dd.MM.yyyy"));
-                            Ausgabe(cos, fontBold, 12, Color.BLACK, 500, 755, "Seite " + Integer.toString(seite));
-                            Ausgabe(cos, fontBold, 12, Color.BLACK, 56, 735, "RechNr");
-                            Ausgabe(cos, fontBold, 12, Color.BLACK, 200, 735, "Lieferant");
-                            Ausgabe(cos, fontBold, 12, Color.BLACK, 500, 735, "Betrag");
+                            AusgabeLB(cos, fontBold, 12, Color.BLACK, 56, 770, "Übersicht der offenen Rechnungen/Ausgaben im Zeitraum " + strVon + " - " + strBis);
+                            AusgabeLB(cos, fontBold, 12, Color.BLACK, 56, 755, "Stand " + ModulHelferlein.printSimpleDateFormat("dd.MM.yyyy"));
+                            AusgabeLB(cos, fontBold, 12, Color.BLACK, 500, 755, "Seite " + Integer.toString(seite));
+                            AusgabeLB(cos, fontBold, 12, Color.BLACK, 56, 735, "RechNr");
+                            AusgabeLB(cos, fontBold, 12, Color.BLACK, 200, 735, "Lieferant");
+                            AusgabeLB(cos, fontBold, 12, Color.BLACK, 500, 735, "Betrag");
 
                             Linie(cos,2,56, 730, 539, 730);
                             
@@ -183,10 +183,10 @@ public class berZahlungen {
                         // Gesamtsumme berechnen
                         Gesamtsumme = Gesamtsumme + Gesamtzeile;
 
-                        // Ausgabe RechNr, Kunde, Betrag    
-                        Ausgabe(cos, fontPlain, 12, Color.BLACK, 56, 715 - 15 * (zeile - 1), resultAusgaben.getString("AUSGABEN_RECHNNR"));
-                        Ausgabe(cos, fontPlain, 12, Color.BLACK, 200, 715 - 15 * (zeile - 1), resultAusgaben.getString("AUSGABEN_LIEFERANT"));
-                        AusgabeDB(cos, fontPlain, 12, Color.BLACK, 520, 715 - 15 * (zeile - 1), Modulhelferlein.df.format(Modulhelferlein.round2dec(Gesamtzeile)));
+                        // AusgabeLB RechNr, Kunde, Betrag    
+                        AusgabeLB(cos, fontPlain, 12, Color.BLACK, 56, 715 - 15 * (zeile - 1), resultAusgaben.getString("AUSGABEN_RECHNNR"));
+                        AusgabeLB(cos, fontPlain, 12, Color.BLACK, 200, 715 - 15 * (zeile - 1), resultAusgaben.getString("AUSGABEN_LIEFERANT"));
+                        AusgabeDB(cos, fontPlain, 12, Color.BLACK, 520, 715 - 15 * (zeile - 1), ModulHelferlein.df.format(ModulHelferlein.round2dec(Gesamtzeile)));
 
                         zeile = zeile + 1;
                     } // while bestellungen
@@ -195,8 +195,8 @@ public class berZahlungen {
                     cos.closeAndStroke();
                     zeile = zeile + 1;
 
-                    Ausgabe(cos, fontPlain, 12, Color.BLACK, 56, 715 - 15 * (zeile - 1), "Gesamtsumme der Zahlungen : ");
-                    AusgabeDB(cos, fontPlain, 12, Color.BLACK, 520, 715 - 15 * (zeile - 1), Modulhelferlein.df.format(Modulhelferlein.round2dec(Gesamtsumme)));
+                    AusgabeLB(cos, fontPlain, 12, Color.BLACK, 56, 715 - 15 * (zeile - 1), "Gesamtsumme der Zahlungen : ");
+                    AusgabeDB(cos, fontPlain, 12, Color.BLACK, 520, 715 - 15 * (zeile - 1), ModulHelferlein.df.format(ModulHelferlein.round2dec(Gesamtsumme)));
 
                     // close the content stream for page 
                     cos.close();
@@ -205,12 +205,12 @@ public class berZahlungen {
                     document.save(outputFileName);
                     document.close();
 
-                    Modulhelferlein.Infomeldung(
+                    ModulHelferlein.Infomeldung(
                             "Liste der offenen Rechnungen/Ausgaben/Zahlungen ist als PDF gespeichert!");
                     try {
                         Runtime.getRuntime().exec("cmd.exe /c " + "\"" + outputFileName + "\"");
                     } catch (IOException exept) {
-                        Modulhelferlein.Fehlermeldung("Exception: " + exept.getMessage());
+                        ModulHelferlein.Fehlermeldung("Exception: " + exept.getMessage());
                     } // try PDF anzeigen
 
                     resultAusgaben.close();
@@ -218,12 +218,12 @@ public class berZahlungen {
                     conn.close();
 
                 } catch (SQLException exept) {
-                    Modulhelferlein.Fehlermeldung("SQL-Exception: SQL-Anfrage nicht moeglich: " + exept.getMessage());
+                    ModulHelferlein.Fehlermeldung("SQL-Exception: SQL-Anfrage nicht moeglich: " + exept.getMessage());
                 } // try 
 
             } // if 
         } catch (IOException e) {
-            Modulhelferlein.Fehlermeldung("IO-Exception: " + e.getMessage());
+            ModulHelferlein.Fehlermeldung("IO-Exception: " + e.getMessage());
         } // try
 
     } // void
